b1ca011048630dab98c0086fd0febc03bcb0a0d2
Michal Hajas <mhajas@redhat.com>
2021-11-29 14:46:36 +0100

    Make lock screen less transparent

--- sway/lock.sh
+++ sway/lock.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 if ! pgrep -u $UID ^swaylock$; then
 	swaymsg mode locked \
-		&& swaylock -s center -i ~/.files/sway/lock.png \
+		&& swaylock -s center -i ~/.files/sway/lock-less-visible.png \
 		&& swaymsg mode default
 fi
