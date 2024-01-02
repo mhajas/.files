de03ace2f7184261e1c3c1d2368917fca899d59a
Michal Hajas <mhajas@redhat.com>
2024-01-02 10:04:05 +0100

    Migrate alacritty to toml

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ALACRITTY_CONFIG=~/.config/alacritty/alacritty.yml
+ALACRITTY_CONFIG=~/.config/alacritty/alacritty.toml
 mkdir -p $(dirname $ALACRITTY_CONFIG)
-ln -s -f $DIR/alacritty.yml $ALACRITTY_CONFIG
+ln -s -f $DIR/alacritty.toml $ALACRITTY_CONFIG
 
